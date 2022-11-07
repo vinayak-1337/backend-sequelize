e17da276bc92da2785f0f9e7ea5811aa12b2dcdf
vinayak <vinayak@gkmit.co>
2022-11-07 16:48:10 +0530

    Add login exeption handling

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,42 +1,71 @@
+import * as dotenv from "dotenv";
+dotenv.config();
 import User from "../models/user.model.js";
 import Account from "../models/account.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const create = async (req, res) => {
   const { name, age, contact, username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
   try {
     const result = await User.create({
       name,
       age,
       contact,
       username,
-      password,
+      password: hashedPassword,
     });
     console.log(result.id);
     const result2 = await Account.create({
       user_id: result.id,
     });
     // res.send(`User created : ${JSON.stringify(result)}`);
-    res.status(200).send(result);
+    return res.status(200).send(result);
   } catch (error) {
     if (error.name === "SequelizeUniqueConstraintError") {
       res.status(400).send("username already exists");
       console.log("Username exists");
     }
+    return res.sendStatus(500);
   }
 };
 
 export const login = async (req, res) => {
   const { username, password } = req.body;
+  const userData = {};
   try {
-    const result = await User.findOne({
+    const userResult = await User.findOne({
       where: {
         username,
-        password,
       },
     });
-    console.log("Login successfully : ", result);
-    res.send(result);
+    if (!userResult) {
+      return res.status(401).send("incorrect username or password");
+    }
+    if (await bcrypt.compare(password, userResult.password)) {
+      const accountResult = await Account.findOne({
+        where: {
+          user_id: userResult.id,
+        },
+      });
+      const userData = {
+        id: userResult.id,
+        name: userResult.name,
+        username: userResult.username,
+        age: userResult.age,
+        balance: accountResult.balance,
+        accountNumber: accountResult.account_number,
+      };
+      const accessToken = jwt.sign(userData, process.env.ACCESS_TOKEN_SECRET);
+      return res.status(201).send({
+        userData,
+        accessToken,
+      });
+    } else {
+      return res.status(401).send("incorrect username or password");
+    }
   } catch (error) {
-    console.log("Error in login : ", error);
+    return res.sendStatus(500);
   }
 };
